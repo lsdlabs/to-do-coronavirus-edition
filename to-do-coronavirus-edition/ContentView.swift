6141d0d96c7975c77f914e3ef004cfe3451442bc
Lauren <laurkeizsmall@gmail.com>
2020-08-12 22:15:27 -0400

    adding My Watchlist Button and changing the Image Set to Render As Original Image

--- to-do-coronavirus-edition/ContentView.swift
+++ to-do-coronavirus-edition/ContentView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        Button(action: {
+            print("My Watchlist Button tapped")
+        }) {
+            Image("My Watchlist Button")
+        }
     }
 }
 
