86c61f38d1fa20cc2b6798617c9cc57484183a48
Lauren <laurkeizsmall@gmail.com>
2020-11-02 21:45:35 -0500

    implementing feedback regarding network request failure

--- to-do-coronavirus-edition/Services/MovieStore.swift
+++ to-do-coronavirus-edition/Services/MovieStore.swift
@@ -10,76 +10,67 @@ import Foundation
 //MovieStore: concrete class that will implement the MovieService
 
 class MovieStore: MovieService {
-
+    
     static let shared = MovieStore()
     private init() {}
-
+    
     private let apiKey = "8d350c8f38888182efa6a31b35171f21"
     private let baseAPIURL = "https://api.themoviedb.org/3"
     private let urlSession = URLSession.shared
     private let jsonDecoder = Helpers.jsonDecoder
-
-
+    
     func searchMovie(query: String, completion: @escaping (Result<MovieResponse, MovieError>) -> ()) {
-        //TO-DO: add method body/implementation
-//        guard let url = URL(string: "\(baseAPIURL)/search/movie") else {
-//            completion(.failure(.invalidEndpoint))
-//            return
-//        }
-
-        let myURL = URL(string: "\(baseAPIURL)/search/movie/?api_key=\(apiKey)&query=\(query)")
-        print(myURL) //myURL is nil
-
-        guard let url = URL(string: "\(baseAPIURL)/search/movie/?api_key=\(apiKey)&query=\(query)") else {
+        guard let encodedQuery = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        
+        let myURL = URL(string: "\(baseAPIURL)/search/movie/?api_key=\(apiKey)&query=\(encodedQuery)")
+        
+        guard let url = myURL else {
             completion(.failure(.invalidEndpoint))
             return
         }
         
-//        self.decodeDataFromURL(url: url, parameters: ["query": query], completion: completion)
-        self.decodeDataFromURL(url: url, parameters: ["query": query], completion: completion)
+        decodeDataFromURL(url: url, parameters: ["query": query], completion: completion)
     }
-
-    private func decodeDataFromURL<Response: Decodable>(url: URL, parameters: [String: String]? = nil, completion: @escaping (Result<Response, MovieError>) -> ()) {
+    
+    private func decodeDataFromURL<Response: Decodable>(url: URL,
+                                                        parameters: [String: String]? = nil,
+                                                        completion: @escaping (Result<Response, MovieError>) -> ()) {
         guard var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
             completion(.failure(.invalidEndpoint))
             return
         }
-
+        
         var queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
-
+        
         if let parameters = parameters {
             queryItems.append(contentsOf: parameters.map { URLQueryItem(name: $0.key, value: $0.value) })
         }
-
+        
         urlComponents.queryItems = queryItems
-
+        
         guard let composedURL = urlComponents.url else {
             completion(.failure(.invalidEndpoint))
             return
         }
-
+        
         urlSession.dataTask(with: composedURL) { (data, response, error) in
-
+            
             if error != nil {
                 completion(.failure(.apiError))
                 return
             }
-
-            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-                //TO-DO: implement error
-                return
+            
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode,
+                let data = data else {
+                    completion(.failure(.invalidResponse))
+                    return
             }
-
-            guard let data = data else {
-                //TO-DO: implement error
-                return
-            }
-
+            
             do {
                 let decodedResponse = try self.jsonDecoder.decode(Response.self, from: data)
-                //TO-DO: implement success
+                completion(.success(decodedResponse))
             } catch {
-                //TO-DO: implement error
+                completion(.failure(.invalidResponse))
             }
         }.resume()
     }
